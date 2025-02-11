ee722cc35c1d5436529047c31e883b4f456a3087
Edecxo <hunterlogan.eden@gmail.com>
2025-02-10 18:17:43 -0600

    added markdown block splitting

--- src/node_helpers.py
+++ src/node_helpers.py
@@ -1,13 +1,13 @@
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    '''splits nodes based on the delimiter and texttype'''
     text = ""
     ret_nodes = []
     for node in old_nodes:
         text = node.text
         strings =[]
         delimiter_indexes = find_all_chars(text, delimiter)
-        print(delimiter_indexes)
 
         #edge cases
         if len(delimiter_indexes) == 0:
@@ -31,6 +31,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return ret_nodes
 
 def find_all_chars(string, char):
+    '''provide indexes of delimiters for split_nodes_delimiter. provides support for multicharacter delimiters'''
     char_indexes = set()
     pos = 0
     while True:
@@ -56,6 +57,8 @@ def extract_markdown_links(text):
 
 def split_nodes_image(old_nodes):
     import re
+    if old_nodes == []:
+        return []
     new_nodes = []
     for node in old_nodes:
         text = node.text
@@ -66,17 +69,23 @@ def split_nodes_image(old_nodes):
         if extract_markdown_images(node.text) == []:
             new_nodes.append(node)
             continue
-        
-        strings = re.split(r"!\[(.*?)\]\((.*?)\)", text)
-        for i in range(0, len(strings), 3):
-            if strings[i] != "":
-                new_nodes.append(TextNode(strings[i], TextType.NORMAL))
-            if 0 <= i+2 < len(strings):
-                new_nodes.append(TextNode(strings[i+1], TextType.LINK, strings[i+2]))
+
+        matches = re.finditer(r"!\[(.*?)\]\((.*?)\)", text)
+        last_position = 0
+        for match in matches:
+            if match.start() > last_position:
+                new_nodes.append(TextNode(text[last_position:match.start()], TextType.NORMAL))
+            new_nodes.append(TextNode(match[1], TextType.IMAGE, match[2]))
+            last_position = match.end()
+        if last_position < len(text):
+            new_nodes.append(TextNode(text[last_position:], TextType.NORMAL))
+
     return new_nodes
 
 def split_nodes_link(old_nodes):
     import re
+    if old_nodes == []:
+        return []
     new_nodes = []
     for node in old_nodes:
         text = node.text
@@ -88,10 +97,36 @@ def split_nodes_link(old_nodes):
             new_nodes.append(node)
             continue
         
-        strings = re.split(r"\[(.*?)\]\((.*?)\)", text)
-        for i in range(0, len(strings), 3):
-            if strings[i] != "":
-                new_nodes.append(TextNode(strings[i], TextType.NORMAL))
-            if 0 <= i+2 < len(strings):
-                new_nodes.append(TextNode(strings[i+1], TextType.LINK, strings[i+2]))
+        matches = re.finditer(r"(?<!\!)\[(.*?)\]\((.*?)\)", text)
+        last_position = 0
+        for match in matches:
+            if match.start() > last_position:
+                new_nodes.append(TextNode(text[last_position:match.start()], TextType.NORMAL))
+            new_nodes.append(TextNode(match[1], TextType.LINK, match[2]))
+            last_position = match.end()
+        if last_position < len(text):
+            new_nodes.append(TextNode(text[last_position:], TextType.NORMAL))
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.NORMAL)]
+    delimiter_types = {
+        split_nodes_link: TextType.LINK,
+        split_nodes_image: TextType.IMAGE,
+        "**": TextType.BOLD,
+        "*": TextType.ITALIC,
+        "`": TextType.CODE
+    }
+    
+    for delimiter_type in delimiter_types:
+        new_nodes = []
+        if callable(delimiter_type):
+            new_nodes = delimiter_type(nodes)
+        else:
+            new_nodes = split_nodes_delimiter(nodes, delimiter_type, delimiter_types[delimiter_type])
+        nodes = new_nodes
+    
+    return nodes
+
+def markdown_to_blocks(markdown):
+    return list(filter(lambda x: x != "", map(str.strip, markdown.split("\n\n"))))
